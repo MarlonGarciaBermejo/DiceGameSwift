9ac340fe69f11365ab21d04e64c2fc6fb86e2a43
Marlon <marlongarciab@hotmail.com>
2023-04-04 14:08:24 +0200

    Initial Commit

---
+++ DiceGameSwift/DiceGameSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceGameSwiftApp.swift
+//  DiceGameSwift
+//
+//  Created by Marlon Garcia-Bermejo on 2023-04-04.
+//
+
+import SwiftUI
+
+@main
+struct DiceGameSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
